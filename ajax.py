2eae936f461e34d8c5f0d167bb90f55579b51099
Dan <dan.tyan@gmail.com>
2013-01-10 19:01:31 +0600

    ajax decorator rename

--- ajax.py
+++ ajax.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import Http404
 
-from zokiguide.decorators import render_to_json
+from common.decorators import json_response
 
 from . models import GuidebookPost, GuidebookPostImages
 from . forms import ImageUploadForm
 
 @login_required
-@render_to_json
+@json_response
 def image_upload( request ):
 
     id = int( request.POST['post'] )
@@ -41,7 +42,7 @@ def image_upload( request ):
     return data
 
 @login_required
-@render_to_json
+@json_response
 def primary( request ):
 
     post_id = int( request.POST['post_id'] )
